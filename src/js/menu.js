fd40419d6ffb084aa233ba84020335e4d2882076
Igor Stojanovski <cableclark@gmail.com>
2019-08-13 15:46:13 +0200

    align the hero area

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,29 +3,33 @@ let spanovi = document.querySelectorAll(".hamburger");
 let menu = document.querySelector("nav ul");
 let listLinks = document.querySelectorAll("nav ul li");
 
-ham.onclick =  function () {
-   spanovi[0].classList.toggle("rotate");
-   spanovi[1].classList.toggle("disapear");
-   spanovi[2].classList.toggle("derotate");
-   menu.classList.toggle("visibleMenu");
-     };
+function toggleMenu() {
 
-menu.onclick =  function () {
   spanovi[0].classList.toggle("rotate");
   spanovi[1].classList.toggle("disapear");
   spanovi[2].classList.toggle("derotate");
+
   menu.classList.toggle("visibleMenu");
+
+  toggleItems();
 };
-
-
-
-listLinks.forEach(function (item) {
-  item.addEventListener("click", hideMenu);
-});
 
 function hideMenu () {
   menu.classList.toggle("visibleMenu");
+
   spanovi[0].classList.toggle("rotate");
   spanovi[1].classList.toggle("disapear");
   spanovi[2].classList.toggle("derotate");
+
+  toggleItems();
   }
+
+function toggleItems () {
+  listLinks.forEach(function (item) {
+    item.classList.toggle("animated-li");
+  });
+};
+
+ham.onclick =  toggleMenu;
+
+menu.onclick = toggleMenu;
