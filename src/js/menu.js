bdcb54cc8520b6f21b40bc6ed1291cd82cdf460d
Igor Stojanovski <cableclark@gmail.com>
2019-08-14 15:49:33 +0200

    style the biography section

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,8 @@
 let ham = document.querySelector("#ham_menu");
-let spanovi = document.querySelectorAll(".hamburger");
 let menu = document.querySelector("nav ul");
 let listLinks = document.querySelectorAll("nav ul li");
 
 function toggleMenu() {
-
-  spanovi[0].classList.toggle("rotate");
-  spanovi[1].classList.toggle("disapear");
-  spanovi[2].classList.toggle("derotate");
 
   menu.classList.toggle("visibleMenu");
 
@@ -16,10 +11,6 @@ function toggleMenu() {
 
 function hideMenu () {
   menu.classList.toggle("visibleMenu");
-
-  spanovi[0].classList.toggle("rotate");
-  spanovi[1].classList.toggle("disapear");
-  spanovi[2].classList.toggle("derotate");
 
   toggleItems();
   }
